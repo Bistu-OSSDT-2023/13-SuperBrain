d18c863f13d711433f21fb791866f37ecb9eff89
xuxinyue341 <138182922+xuxinyue341@users.noreply.github.com>
2023-07-06 00:07:23 +0800

    Update Client.java

--- SuperBrain/SuperBrain/Client.java
+++ SuperBrain/SuperBrain/Client.java
@@ -3,6 +3,7 @@ package SuperBrain;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
