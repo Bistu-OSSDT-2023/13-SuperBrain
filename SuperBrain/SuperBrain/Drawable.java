806b9b50f8085958aba10bb8818b095248f20e98
ChilliZhao <138182485+ChilliZhao@users.noreply.github.com>
2023-07-05 23:18:58 +0800

    ChilliZhao

---
+++ SuperBrain/SuperBrain/Drawable.java
@@ -0,0 +1,7 @@
+package SuperBrain;
+
+import java.awt.Graphics;
+
+public interface Drawable {
+	void draw(Graphics g) throws CloneNotSupportedException;
+}
---
+++ SuperBrain/SuperBrain/Moveable.java
@@ -0,0 +1,5 @@
+package SuperBrain;
+
+public interface Moveable {
+	void move();
+}
